cedf542ba54d964c52720c64b65801946e310f6d
npablo1987 <kyovilches@gmail.com>
2022-01-18 23:05:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/hola_mundos5/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hola_mundos5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int result = check();
+        Toast.makeText(this, "Hola estoy aprendinedo este curso:"+result, Toast.LENGTH_SHORT).show();
+    }
+
+    private int check() {
+        int a = 5;
+        int b = 20;
+        int c = 20;
+
+        if(a > b ){
+            return 0 ;
+
+        }else{
+            if(c > b){
+                return c;
+            }else
+                return a;
+
+        }
+
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/hola_mundos5/test.java
@@ -0,0 +1,11 @@
+package com.example.hola_mundos5;
+
+
+public class test {
+
+    public static void prueba(){
+
+
+
+    }
+}
